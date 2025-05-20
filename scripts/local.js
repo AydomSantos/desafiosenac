22c2e781eebc4ab87f7d48689ae90f7dbed0119d
CarlosBlack13 <163689188+CarlosBlack13@users.noreply.github.com>
2025-05-20 20:10:03 -0300

    Delete scripts directory

--- scripts/local.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Aplicar tema ao carregar
-    const tema = localStorage.getItem('tema') || 'claro';
-    if (tema === 'escuro') {
-        document.body.classList.add('dark-mode');
-    }
-});
