4261c5327457a2527d405a6d80b535aa043dc9f8
CarlosBlack13 <163689188+CarlosBlack13@users.noreply.github.com>
2025-05-20 19:27:29 -0300

    Delete Desafio_senac directory

--- Desafio_senac/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'config.php';
-
-if (estaLogado()) {
-    redirect('dashboard.php');
-} else {
-    redirect('login.php');
-}
-?>
--- Desafio_senac/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once 'config.php';
-require_once 'classes/Sessao.php';
-
-Sessao::encerrar();
-redirect('login.php');
-?>
